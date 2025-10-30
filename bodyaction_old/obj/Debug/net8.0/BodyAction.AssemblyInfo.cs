98e0b980a70f933c206d1bc0af46a87cac70ec41
KrainskiH <henriquekrainski38@gmail.com>
2025-10-29 21:02:51 -0300

    Arrumar caminho das pastas

--- bodyaction_old/obj/Debug/net8.0/BodyAction.AssemblyInfo.cs
+++ bodyaction_old/obj/Debug/net8.0/BodyAction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BodyAction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3693690d706713cbafb7d1d5374fc762a69c217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09166ce70f74b7ece17506960b43d9acaa2ed5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BodyAction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BodyAction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
